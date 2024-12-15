50eb1b110cf7a9d59f23adfef0cea369993bfddf
Abhishek <abhishekst.2021@gmail.com>
2024-12-16 03:26:01 +0530

    First insert done

--- HibernateLearn/src/main/java/com/demo/App.java
+++ HibernateLearn/src/main/java/com/demo/App.java
@@ -1,6 +1,8 @@
 package com.demo;
 
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
 public class App 
@@ -9,6 +11,19 @@ public class App
     {
         System.out.println( "Hibernate Project started..." );
         SessionFactory factory = new Configuration().configure().buildSessionFactory();
-        System.out.println(factory);
+
+    //Create student
+        Student st = new Student();
+        st.setId(103);
+        st.setName("Aman");
+        st.setCity("Jaipur");
+        
+        System.out.println(st);
+        
+        Session session = factory.openSession();
+        Transaction tx = session.beginTransaction();
+        session.save(st);
+        tx.commit();
+        session.close();
     }
 }
