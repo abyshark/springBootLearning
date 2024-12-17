7b769a35ae05195f00b0db2d1de71a723d302841
Abhishek <abhishekst.2021@gmail.com>
2024-12-17 10:02:11 +0530

    hibernate set

--- HibernateLearn/src/main/java/com/demo/App.java
+++ HibernateLearn/src/main/java/com/demo/App.java
@@ -1,5 +1,10 @@
 package com.demo;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
 /**
  * Hello world!
  *
@@ -8,7 +13,21 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
-        System.out.println("Test git hub account connection!!!");
+        System.out.println( "Hibernate Project started..." );
+        SessionFactory factory = new Configuration().configure().buildSessionFactory();
+
+//    //Create student
+//        Student st = new Student();
+//        st.setId(103);
+//        st.setName("Aman");
+//        st.setCity("Jaipur");
+//        
+//        System.out.println(st);
+        
+        Session session = factory.openSession();
+        Transaction tx = session.beginTransaction();
+//        session.save(st);
+        tx.commit();
+        session.close();
     }
 }
