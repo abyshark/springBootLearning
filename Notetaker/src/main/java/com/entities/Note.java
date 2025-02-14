bd2b094b8693cd4762941e0c02f80c69b282ec3a
Abhishek <abhishekst.2021@gmail.com>
2025-02-14 08:43:16 +0530

    Entity class and factory class added in notetaker

---
+++ Notetaker/src/main/java/com/entities/Note.java
@@ -0,0 +1,39 @@
+package com.entities;
+
+import java.util.Date;
+import java.util.Random;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "notes")
+@Data
+@NoArgsConstructor
+public class Note {
+
+	@Id
+	private int id;
+	private String title;
+	private String content;
+	private Date addedDate;
+	/**
+	 * @param id
+	 * @param title
+	 * @param content
+	 * @param addedDate
+	 */
+	public Note(String title, String content, Date addedDate) {
+		super();
+		this.id = new Random().nextInt(1000);
+		this.title = title;
+		this.content = content;
+		this.addedDate = addedDate;
+	}
+	
+	
+}
