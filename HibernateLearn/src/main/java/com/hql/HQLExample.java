472ed5660d960a8bf4c721feb03e55c5b7a425f0
Abhishek <abhishekst.2021@gmail.com>
2025-03-11 08:58:19 +0530

    normal HQL query

--- HibernateLearn/src/main/java/com/hql/HQLExample.java
+++ HibernateLearn/src/main/java/com/hql/HQLExample.java
@@ -1,10 +1,46 @@
 package com.hql;
 
+import java.util.List;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.demo.Student;
+
 public class HQLExample {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Configuration cfg = new Configuration();
+		cfg.configure("hibernate.cfg.xml");
+		SessionFactory factory = cfg.buildSessionFactory();
+		
+		Session s = factory.openSession();
+		
+		
+		//HQL
+		//Syntax
+		String query= "from Student";
+		String query1= "from Student where city = 'LKO'";
+		Query q = s.createQuery(query);
+		Query q1 = s.createQuery(query1);
+		
+		List<Student> list = q.list();
+		for(Student student : list) {
+			System.out.println(student.toString());
+		}
+		
+		List<Student> list1 = q1.list();
+		for(Student student1 : list1) {
+			System.out.println(student1.getName()+" "+student1.getCity());
+		}
+		
+		
+		s.close();
+		factory.close();
+		
 	}
 
 }
