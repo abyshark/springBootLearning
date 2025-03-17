d7c9b990d5f7b6a2a1fd8ba7a629984e9b2369c2
Abhishek <abhishekst.2021@gmail.com>
2025-03-18 02:57:16 +0530

    Criteria api using Restrictions to fetch data with conditions

--- HibernateLearn/src/main/java/com/criteria/CriteriaExample.java
+++ HibernateLearn/src/main/java/com/criteria/CriteriaExample.java
@@ -8,6 +8,7 @@ import javax.persistence.criteria.CriteriaQuery;
 import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.criterion.Restrictions;
 
 import com.demo.Student;
 
@@ -19,6 +20,8 @@ public class CriteriaExample {
 //		CriteriaQuery<Student> criteria = ss.createQuery(Student.class);
 		
 		Criteria c= s.createCriteria(Student.class);
+//		c.add(Restrictions.eq("name", "Abhishek"));
+		c.add(Restrictions.like("certificate.course", "Ja%"));
 		List<Student> list = c.list();
 		
 		for(Student st: list) {
