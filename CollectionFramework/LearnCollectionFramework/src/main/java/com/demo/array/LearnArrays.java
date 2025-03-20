a25ecf6b7285b665ff6de8ac9f322891ec2d26fb
Abhishek <abhishekst.2021@gmail.com>
2025-03-20 10:06:25 +0530

    Added Arrays file in collection project

--- CollectionFramework/LearnCollectionFramework/src/main/java/com/demo/array/LearnArrays.java
+++ CollectionFramework/LearnCollectionFramework/src/main/java/com/demo/array/LearnArrays.java
@@ -1,7 +1,7 @@
-package com.demo;
+package com.demo.array;
 
 public class LearnArrays {
     public static void main(String[] args) {
-        System.out.println("Arrays class in collection!!");
+        System.out.println("hello World!!");
     }
 }
