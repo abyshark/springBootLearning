add4e1eea77fb56fc373ce85ff0869ae63794b20
Abhishek <abhishekst.2021@gmail.com>
2025-03-21 09:25:45 +0530

    Created different classes in list interface in collection framework

---
+++ CollectionFramework/LearnCollectionFramework/src/main/java/com/demo/array/LearnArrayList.java
@@ -0,0 +1,21 @@
+package com.demo.array;
+
+import java.util.ArrayList;
+import java.util.Vector;
+
+public class LearnArrayList {
+    public static void main(String[] args) {
+        System.out.println("This is array list");
+
+        // declaration of ArrayList
+        ArrayList arrlist = new ArrayList();
+        ArrayList arrlist1 = new ArrayList(30);
+
+        ArrayList<String> arrayList2 = new ArrayList<>();
+
+        Vector v = new Vector();
+        Vector vc = new Vector(60);
+
+
+    }
+}
