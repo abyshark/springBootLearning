2876f367ff6346b72eab510dd1376ae06935b8ce
Abhishek <abhishekst.2021@gmail.com>
2025-03-22 20:21:48 +0530

    Set interface and tree set class declaration added

--- CollectionFramework/LearnCollectionFramework/src/main/java/com/demo/array/LearnArrayList.java
+++ CollectionFramework/LearnCollectionFramework/src/main/java/com/demo/array/LearnArrayList.java
@@ -1,8 +1,6 @@
 package com.demo.array;
 
-import java.util.ArrayList;
-import java.util.Stack;
-import java.util.Vector;
+import java.util.*;
 
 public class LearnArrayList {
     public static void main(String[] args) {
@@ -25,6 +23,44 @@ public class LearnArrayList {
         stack.push(23);
 
         Object obj= stack.pop();
+
+        //cursor in collection framework
+        Scanner sc = new Scanner(System.in);
+        Vector v1 = new Vector();
+        System.out.println("Enter Vector elements");
+        for (int i = 0; i < 5; i++) {
+            v1.add(sc.nextInt());
+        }
+        System.out.println("Vector v1 = :"+v1);
+        Enumeration e = v1.elements();
+
+        while(e.hasMoreElements()){
+            System.out.println(e.nextElement());
+        }
+
+        //  hash set
+        HashSet hs = new HashSet();
+        HashSet hs1 = new HashSet(100,.80f); // here if 80 percent of the hash set is filled then the new hash set is created
+
+        ArrayList ls = new ArrayList();
+        HashSet hs4 = new HashSet(ls); // here arraylist hashset is created and stored in hs4 hashSet
+
+        //linkedHashSet
+
+        LinkedHashSet lhs = new LinkedHashSet();
+        LinkedHashSet lhs1 = new LinkedHashSet(100);
+        LinkedHashSet lhs2 = new LinkedHashSet(100,.80f);
+
+        // combination of linked list and hashset
+        //insertion order is conserved
+
+        //Sorted set
+        SortedSet s = new TreeSet(); // Tree set is the implementation of interface sorted Set
+
+        //Tree set
+        TreeSet tree = new TreeSet(); // declaration of tree set
+
+
 
 
     }
